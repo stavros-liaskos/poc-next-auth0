59a87feed878486d333dc90c7118204b1686d23a
Stavros Liaskos <st.liaskos@gmail.com>
2025-04-04 16:47:27 +0200

    s

--- app/external/page.jsx
+++ app/external/page.jsx
@@ -14,7 +14,7 @@ export default function External() {
     setState(previous => ({ ...previous, isLoading: true }));
 
     try {
-      const response = await fetch('/test-ui/test-ui');
+      const response = await fetch('https://api.releaseraccoon.online/test-ui/test-ui');
       const data = await response.json();
 
       setState(previous => ({ ...previous, response: data, error: undefined }));
@@ -28,7 +28,7 @@ export default function External() {
     setState(previous => ({ ...previous, isLoading: true }));
 
     try {
-      const response = await fetch('/artist/search?pattern=sear');
+      const response = await fetch('https://api.releaseraccoon.online/artist/search?pattern=sear');
       const data = await response.json();
 
       setState(previous => ({ ...previous, response: data, error: undefined }));
